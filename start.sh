dc1b45d65264070119925f158698081335dba80f
mrbillington <ctown41@gmail.com>
2017-05-17 18:58:07 -0700

    Fixes

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 set -e
 
-ln -s /config/saves /factorio/saves
-cp -rp /files/ /conifg
+ln -sf /config/saves /factorio/saves
+cp -rp /files /conifg
 
 if ! find -L /factorio/saves -iname \*.zip -maxdepth 1 -print | grep -q . ; then
 	echo "No save files found. Creating new map."
