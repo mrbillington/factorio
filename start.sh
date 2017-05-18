a25341db25e1aec3c6a0edd31f96c5e6ae0bbb05
mrbillington <ctown41@gmail.com>
2017-05-17 18:22:56 -0700

    Fixes

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ mkdir -p /factorio/saves
 ln -s /factorio/saves /config/saves
 cp -rp /files/ /conifg
 
+ls -la /config
+ls -la /files
+ls -la /factorio
+
 if ! find -L /factorio/saves -iname \*.zip -mindepth 1 -print | grep -q . ; then
 	echo "No save files found. Creating new map."
 	/factorio/bin/x64/factorio --create /factorio/saves/_autosave1.zip --map-gen-settings /config/map-gen-settings.json
